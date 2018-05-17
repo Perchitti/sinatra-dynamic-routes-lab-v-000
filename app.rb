97f49b77ae84bbd83014a930dbf50d79deb840d1
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-17 18:20:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,13 +23,22 @@ end
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = "*" || "+" || "-" || "/"
     @num_1 = params[:number1].to_i
     @num_2 = params[:number2].to_i
     case params[:operation].downcase
     when 'add' || '+'
       (@num1 + @num2).to_s
     when 'subtract' || '-'
+      (@num1 - @num2).to_s
+    when 'multiply' || '*'
+      (@num1 * @num2).to_s
+    when 'divide' || '/'
+      (@num1 / @num2).to_s
+    else
+      "Error"
+    end
+  end
+
 
     end
 
